1c1b4214f3fe62adab85857023edab53d151eef4
Srinivas Tamada <srinivas@9lessons.info>
2017-03-20 23:01:14 -0400

    final change

--- src/components/appBody.js
+++ src/components/appBody.js
@@ -1,6 +1,10 @@
-import React, {Component} from 'react';
+import React, {
+    Component
+} from 'react';
 import AppBodyData from './appBodyData';
-import {Content} from 'native-base';
+import {
+    Content
+} from 'native-base';
 export default class AppBody extends Component {
     constructor() {
         super()
@@ -9,27 +13,33 @@ export default class AppBody extends Component {
         }
     }
     getData() {
-        fetch('http://localhost/test/test.php').then((response) => response.json()).then((responseJson) => {
-           
-            this.setState({data: responseJson.feed.entry });
+        fetch('https://www.thewallscript.com/blogfeed/javascript/10', {
+                method: 'GET'}).then((response) => response.json()).then((responseJson) => {
 
-        }).catch((error) => {
-            console.error(error);
-        });
+                this.setState({
+                    data: responseJson.feed.entry
+                });
+
+            }).catch((error) => {
+                console.error(error);
+            });
+        }
+
+        componentDidMount() {
+
+            this.getData();
+
+        }
+
+        render() {
+
+            return ( <
+                AppBodyData data = {
+                    this.state.data
+                }
+                />
+            );
+        }
     }
 
-    componentDidMount() {
-       
-        this.getData();
-
-    }
-
-    render() {
-
-        return (
-            <AppBodyData data={this.state.data}/>
-        );
-    }
-}
-
-module.export = AppBody;
+    module.export = AppBody;
